4026e6c99dfea69497629b1f4b807bd80c75e156
PelleLindroth <pellelindroth@gmail.com>
2021-03-11 16:00:54 +0100

    Add redirect and wildcard 404 route

--- app.js
+++ app.js
@@ -9,11 +9,19 @@ app.use(express.static('public'))
 
 app.get('/albums/:page', (req, res) => {
   let page = req.params.page
+
+  if (page < 1 || page > totalPages) {
+    res.redirect('/albums/1')
+  }
   res.render('index', {
     albums: albums.slice((page * 10) - 10, page * 10),
     next: page <= totalPages ? `/albums/${+page + 1}` : null,
     previous: page == 1 ? null : `/albums/${+page - 1}`
   })
+})
+
+app.get('/*', (req, res) => {
+  res.status(404).send('<h1>Page Not Found!</h1>')
 })
 
 app.listen(PORT, () => {
